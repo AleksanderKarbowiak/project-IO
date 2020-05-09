e5773219bcad5b78bfbf5743da4df5997e4dcd49
YeahBunnyGr00Bass <64377342+YeahBunnyGr00Bass@users.noreply.github.com>
2020-05-09 18:29:27 +0200

    Update DTFT.java

--- Apka/src/Classes/DTFT.java
+++ Apka/src/Classes/DTFT.java
@@ -10,12 +10,26 @@ public class DTFT {
   //
   // Fields
   //
-
+    private final double CzęstotliwośćPoczątkowa;
+    private final double CzęstotliwośćKońcowa;
+    private final double KrokCzęstotliwości;
+    private final int LiczbaPróbek;
+    private final double CzęstotliwośćPróbkowania;
   
   //
   // Constructors
   //
-  public DTFT () { };
+  public DTFT(double początek, double koniec, double krok, double fs)
+    {
+        CzęstotliwośćPoczątkowa = początek;
+        CzęstotliwośćKońcowa = koniec;
+        KrokCzęstotliwości = krok;
+        
+        LiczbaPróbek = (int)((koniec - początek)/krok);
+        
+        CzęstotliwośćPróbkowania = fs;
+        
+    }//Koniec konstruktora
   
   //
   // Methods
@@ -34,9 +48,73 @@ public class DTFT {
    * @return       object
    * @param        recording
    */
-  public object calculateSpectrum(Recording recording)
-  {
-  }
+ private double pulsacja(double f)
+    {
+        double wynik;
+        
+        wynik = 2 * Math.PI * f;
+        
+        return wynik;
+    }//Koniec obliczania pulsacji
+
+ private Complex ObliczPróbkęWidma(double f, double[] próbki)
+    {
+        int i;
+        Complex wynik;
+        double a;
+        double b;
+        double omega;
+        double ułamek;
+        
+        a = 0;
+        b = 0;
+        omega = pulsacja(f);
+        
+        for(i = 0; i < próbki.length; i++)
+        {
+            ułamek = (omega * (double)i)/CzęstotliwośćPróbkowania;
+            
+            a += próbki[i] * Math.cos(ułamek);
+            b -= próbki[i] * Math.sin(ułamek);
+        }//next i
+        
+        a = a/(double)próbki.length;
+        b = b/(double)próbki.length;
+        
+        wynik = new Complex(a,b);
+        
+        return wynik;
+        
+    }//Koniec obliczania pojedynczej próbki widma
+    
+    
+    public widmo[] ObliczDTFT(double[] próbki)
+    {
+        widmo[] tablica;
+        int i;
+        double f;
+        Complex pojedynczy;
+        double a;
+        double b;
+        
+        tablica = new widmo[LiczbaPróbek];
+        
+        for(i = 0; i < LiczbaPróbek; i++)
+        {
+            f = CzęstotliwośćPoczątkowa + (KrokCzęstotliwości * ((double)i));
+            
+            pojedynczy = ObliczPróbkęWidma(f, próbki);
+            
+            a = pojedynczy.getA();
+            b = pojedynczy.getB();
+            
+            tablica[i] = new widmo(a, b, f);
+            
+        }//next i
+        
+        return tablica;
+        
+    }//Koniec obliczania widma
 
 
 }
