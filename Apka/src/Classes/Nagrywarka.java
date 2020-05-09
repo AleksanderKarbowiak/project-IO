17ed4ba96b2a5b7044821ceeae7ce82776abf80a
baltek99 <64107123+baltek99@users.noreply.github.com>
2020-05-09 17:09:59 +0200

    Update Nagrywarka.java

--- Apka/src/Classes/Nagrywarka.java
+++ Apka/src/Classes/Nagrywarka.java
@@ -1,101 +1,89 @@
 package Classes;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Class Nagrywarka
  */
 public class Nagrywarka {
 
-  //
-  // Fields
-  //
+    File wavFile;
+    /** Format audio - .WAV */
+    AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+    TargetDataLine line;
+    /** Scieżka do Bazy Nagrań w której umieszczane będą pliki audio */
+    String path = "Tutaj ścieżka do folderu";
 
-  public Baza_danych baza_nagran;
-  public Kalibracja kalibracja;
+    /**
+     * Metoda wywoływująca funkcję rozpoczynająca nagrywanie i zwracająca obiekt klasy Nagranie
+     * @param nazwa - nazwa nagrania
+     * @param imie - imie użytkownika
+     * @param nazwisko - nazwisko użytkownika
+     * @return nagranie
+     */
+
+    Nagranie Nagraj(String nazwa, String imie, String nazwisko) {
+        Nagranie nagranie = new Nagranie(nazwa, imie, nazwisko);
+        wavFile = new File(path + nazwa + ".wav");
+        Start();
+        return nagranie;
+    }
+
+    /**
+     * Metoda rozpoczynająca nagrywanie
+     */
+    void Start() {
+        try {
+            /** Format audio - częstotliwość próbkowania, rozmiar próbki w bitach, wybór Mono/Stereo */
+            AudioFormat format = new AudioFormat(16000, 8, 2, true, true);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+            /** Wyjątek w przypadku błędu przy połączeniu z mikrofonem (np. brak mikrofonu) */
+            if (!AudioSystem.isLineSupported(info)) {
+                System.out.println("Line not supported");
+                System.exit(0);
+            }
+
+            /** Rozpoczęcie nagrywania */
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+            line.start();
+            //System.out.println("Przechwytywanie");
+            AudioInputStream ais = new AudioInputStream(line);
+            System.out.println("Nagrywanie rozpoczęte");
+
+            AudioSystem.write(ais, fileType, wavFile);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Metoda kończąca nagrywanie
+     */
+    void Stop() {
+        line.stop();
+        line.close();
+        System.out.println("Zakończono nagrywanie");
+    }
   
-  //
-  // Constructors
-  //
-  public Nagrywarka () { };
-  
-  //
-  // Methods
-  //
+    /**
+     * Set the value of path
+     * @param newVar the new value of path
+     */
+    public void setPath (String newVar) {
+        path = newVar;
+    }
 
-
-  //
-  // Accessor methods
-  //
-
-  /**
-   * Set the value of baza_nagran
-   * @param newVar the new value of baza_nagran
-   */
-  public void setBaza_nagran (Baza_danych newVar) {
-    baza_nagran = newVar;
-  }
-
-  /**
-   * Get the value of baza_nagran
-   * @return the value of baza_nagran
-   */
-  public Baza_danych getBaza_nagran () {
-    return baza_nagran;
-  }
-
-  /**
-   * Set the value of kalibracja
-   * @param newVar the new value of kalibracja
-   */
-  public void setKalibracja (Kalibracja newVar) {
-    kalibracja = newVar;
-  }
-
-  /**
-   * Get the value of kalibracja
-   * @return the value of kalibracja
-   */
-  public Kalibracja getKalibracja () {
-    return kalibracja;
-  }
-
-  //
-  // Other methods
-  //
-
-  /**
-   * wstrzymuje nagrywanie
-   */
-  public void Pauza()
-  {
-  }
-
-
-  /**
-   * Ko�czy nagrywanie
-   */
-  public void Stop()
-  {
-  }
-
-
-  /**
-   * Usuwa nagranie o podanej nazwie
-   * @param nazwa
-   */
-  public void UsunNagranie(String nazwa)
-  {
-  }
-
-
-  /**
-   * Funkcja rozpoczynaj�ca nagrywanie oraz zwracaj�ca obiekt nagranie
-   * @return       Nagranie
-   * @param        nazwa
-   * @param        informacje
-   */
-  public Nagranie Nagraj(String nazwa, String informacje)
-  {
-  }
-
-
+    /**
+     * Get the value of path
+     * @return the value of path
+     */
+    public String getPath () {
+        return path;
+    }
 }
