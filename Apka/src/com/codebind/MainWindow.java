86e536796620cb5e9c08f2b32bcdc1955b20db57
AleksanderKarbowiak <63404639+AleksanderKarbowiak@users.noreply.github.com>
2020-05-15 16:05:20 +0200

    Nagrywanie podłączone do guziczków

--- Apka/src/com/codebind/MainWindow.java
+++
@@ -1,4 +0,0 @@
-package com.codebind;
-
-public class MainWindow {
-}
---
+++ AplikacjaDoNagrywaniaAnalizyDzwieku/src/Classes/PanelSygnalizujacyZDiodami.java
@@ -0,0 +1,51 @@
+package Classes;
+
+/**
+ * Class PanelSygnalizujacyZDiodami
+ */
+public class PanelSygnalizujacyZDiodami {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public PanelSygnalizujacyZDiodami () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       boolean
+   */
+  public boolean wlaczony()
+  {
+    //TODO
+    return true;
+  }
+
+
+  /**
+   * @return       boolean
+   */
+  public boolean rejestracjaDzwieku()
+  {
+    //TODO
+    return true;
+  }
+
+
+}
