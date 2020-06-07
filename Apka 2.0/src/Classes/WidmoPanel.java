01199e727f922570ade94007ea5a4b8c70296dae
BartłomiejLachowiecki <64107123+baltek99@users.noreply.github.com>
2020-06-07 16:51:19 +0200

    Wykres widma DTFT od Krystiana

---
+++ Apka 2.0/src/Classes/WidmoPanel.java
@@ -0,0 +1,107 @@
+package Classes;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class WidmoPanel extends JFrame{
+    private JComboBox comboBox1;
+    private JButton okButton;
+    private JLabel name;
+    private JPanel text;
+    public JPanel WidmoPanel;
+    private JTextField textField1;
+    private JSpinner FrequencySpinner;
+    private JTextField textField3;
+    private Baza_danych baza;
+
+    private int iloscProbek;
+    private int czestotliwosc;
+    private int amplituda;
+
+    public WidmoPanel(String text, Baza_danych baza) {
+        super(text);
+        this.baza = baza;
+        for(Nagranie nagranie : baza.nagrania) {
+            comboBox1.addItem(nagranie.nazwa);
+        }
+        SpinnerModel model = new SpinnerNumberModel(20, 20, 8000, 1);
+        FrequencySpinner.setModel(model);
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                czestotliwosc = (Integer) FrequencySpinner.getValue();
+                wyswietlWidmo();
+            }
+        });
+    }
+
+    public void wyswietlWidmo() {
+        Ramka Okno;
+        DTFT Transformata;
+
+        double fs; //Parametry próbkowania
+        double f_max; //Parametry DTFT
+        double f_min;
+        double f_krok;
+
+        widmo[] wynik = new widmo[2048];
+
+        String file = (String) comboBox1.getSelectedItem();
+        PlikWave plik = new PlikWave( file + ".wav");
+        plik.OtwórzIstniejącyPlik();
+
+        fs = plik.getCzęstotliwośćPróbkowania();
+        f_max = fs/2;
+        f_min = czestotliwosc;
+        f_krok = fs/2048;
+        Transformata = new DTFT(f_min, f_max, f_krok, fs);
+
+        int ileProbek = 4096;
+        int indexProbki = 0;
+        double[] probkiSuma = new double[2048];
+
+        while (indexProbki + ileProbek < plik.getLiczbęPróbek())
+        {
+            byte[] probki = plik.PobierzKilkaPróbek(indexProbki, ileProbek);
+            indexProbki += ileProbek;
+
+            double[] probkiL = new double[ileProbek/2];
+            int licznikL = 0;
+            for (int i = 0; i < probkiL.length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    probkiL[licznikL++] = probki[i];
+                }
+            }
+
+            for (int i = 0; i < probkiSuma.length; i++)
+            {
+                probkiSuma[i] += probkiL[i];
+            }
+        }
+
+        wynik = Transformata.ObliczDTFT(probkiSuma);
+
+        /*Okno = new Ramka(600,600,"Wykres widma",10,10, wynik);
+        Okno.UstawMnieNaŚrodku();
+        Okno.setVisible(true);
+        */
+        WykresWidma wykres = new WykresWidma(wynik, "Widmo");
+        wykres.setSize(800, 400);
+        wykres.setLocationRelativeTo(null);
+        wykres.setVisible(true);
+        //WyprowadźWidmo(wynik);
+
+    }
+
+}
---
+++ Apka 2.0/src/Classes/WykresWidma.java
@@ -0,0 +1,105 @@
+package Classes;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+
+public class WykresWidma extends JFrame {
+
+    public WykresWidma(widmo[] tab, String title) {
+
+        initUI(tab, title);
+    }
+
+    private void initUI(widmo[] tab, String title) {
+
+        //XYDataset dataset = createDataset(tab);
+        XYDataset dataset = createDataset(tab);
+        JFreeChart chart = createChart(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
+
+        pack();
+        setTitle(title);
+        setLocationRelativeTo(null);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private XYDataset createDataset(widmo[] tab) {
+
+        /*var seria = new XYSeries("Widmo");
+
+        double fs = 16000;
+        double krok = fs/tab.length;
+        double f = 0;
+        for (int i =0; i< tab.length/2; i++)
+        {
+            seria.add(f, tab[i]);
+            f += krok;
+        }
+         */
+        var seria = new XYSeries("Widmo");
+        for(int i = 0; i < tab.length; i++) {
+            seria.add(tab[i].getCzęstotliwość(), tab[i].get_dB());
+        }
+
+        var dataset = new XYSeriesCollection();
+        dataset.addSeries(seria);
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Widmo",
+                "Częstotliwość [Hz]",
+                "[dB]",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        /*var renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.BLUE);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+*/
+        //plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle("Widmo",
+                        new Font("Serif", java.awt.Font.BOLD, 18)
+                )
+        );
+
+        return chart;
+    }
+
+}
