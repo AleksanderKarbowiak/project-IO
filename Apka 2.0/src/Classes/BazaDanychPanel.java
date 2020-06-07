cffbf0cd4cb809c05be5d01cdf72924a9c7bc13b
BartłomiejLachowiecki <64107123+baltek99@users.noreply.github.com>
2020-06-07 16:30:51 +0200

    Add files via upload

---
+++ Apka 2.0/src/Classes/BazaDanychPanel.java
@@ -0,0 +1,30 @@
+package Classes;
+
+import javax.swing.*;
+import javax.swing.table.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BazaDanychPanel extends JFrame {
+    public JPanel DatabasePanel;
+    private JLabel label;
+    private JTable databaseTable;
+    private JList list1;
+
+    public BazaDanychPanel(String title, Baza_danych database) {
+        super(title);
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Nazwa");
+        model.addColumn("Imie");
+        model.addColumn("Nazwisko");
+        model.addColumn("Wiek");
+        model.addColumn("Płeć");
+        model.addRow(new Object[]{"Nazwa","Imię","Nazwisko","Wiek","Płeć"});
+        for (Nagranie nagranie : database.nagrania()) {
+            model.addRow(new Object[]{ nagranie.nazwa, nagranie.imie, nagranie.nazwisko,nagranie.wiek,nagranie.plec });
+            databaseTable.setModel(model);
+        }
+    }
+
+
+}
