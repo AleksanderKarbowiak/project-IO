76b1cc680bcacfd0922bb1a685ad0bc549fd37e2
BartłomiejLachowiecki <64107123+baltek99@users.noreply.github.com>
2020-06-07 16:57:16 +0200

    dodanie wieku i płci

--- Apka 2.0/src/Classes/Nagranie.java
+++ Apka 2.0/src/Classes/Nagranie.java
@@ -8,19 +8,24 @@ public class Nagranie implements Serializable {
     String nazwa;
     String imie;
     String nazwisko;
-
+    String wiek;
+    String plec;
     /**
      * Konstruktor klasy Nagranie
      *
      * @param nazwa - nazwa nagrania
      * @param imie - imie użytkownika
      * @param nazwisko - nazwisko użytkownika
+     * @param wiek - wiek użytkownika
+     * @param plec - płeć użytkownika
      */
 
-    Nagranie(String nazwa, String imie, String nazwisko) {
+    Nagranie(String nazwa, String imie, String nazwisko, String wiek,String plec) {
         this.nazwa = nazwa;
         this.imie = imie;
         this.nazwisko = nazwisko;
+        this.wiek = wiek;
+        this.plec = plec;
     }
     public String getImie(){
         return imie;
@@ -34,9 +39,12 @@ public class Nagranie implements Serializable {
         return nazwa;
     }
 
+    public  String getWiek(){return wiek;}
+
+    public  String getPlec(){return plec;}
     //metoda do wypisywania zawartości obiektu
     public void getNagranieData(){
-        System.out.print(getImie() + " " + getNazwisko()+ " " + getNazwa() +'\n');
+        System.out.print(getImie() + " " + getNazwisko()+ " " + getNazwa() + " " + getWiek() + " " + getPlec() + '\n');
     }
 
 }
