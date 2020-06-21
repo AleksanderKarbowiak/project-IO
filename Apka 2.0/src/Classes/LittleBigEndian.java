55b9de2194e2426dd59be9fc4345efb4a9dc9595
AleksanderKarbowiak <63404639+AleksanderKarbowiak@users.noreply.github.com>
2020-06-21 18:49:59 +0200

    Update LittleBigEndian.java

--- Apka 2.0/src/Classes/LittleBigEndian.java
+++ Apka 2.0/src/Classes/LittleBigEndian.java
@@ -10,6 +10,7 @@ import java.nio.ByteOrder;
 
 /**
  * Klasa służąca do zamiany Small Endian na BigEndian i odwrotnie.
+ * Udostępniona do użytku przez dr Piotra Wrzeciono
  * Klasa zawiera tylko metody statyczne!
  *
  * @author Piotr Wrzeciono
